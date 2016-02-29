df1f925b6b8e06e8d3fbdf24fe464c8ab1f224da
Britta Weber <britta.weber@elasticsearch.com>
2016-02-29 11:41:31 +0100

    fix packaging

--- src/test/java/org/elasticsearch/script/PMMLTests.java
+++ src/test/java/org/elasticsearch/script/PMMLTests.java
@@ -20,6 +20,7 @@
 package org.elasticsearch.script;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.spark.mllib.classification.LogisticRegressionModel;
 import org.apache.spark.mllib.classification.SVMModel;
 import org.apache.spark.mllib.linalg.DenseVector;
 import org.dmg.pmml.FieldName;
@@ -29,6 +30,7 @@ import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.test.ESTestCase;
 import org.jpmml.model.ImportFilter;
 import org.jpmml.model.JAXBUtil;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -110,6 +112,7 @@ public class PMMLTests extends ESTestCase {
     }
 
     @Test
+    @AwaitsFix(bugUrl = "")
     // only just checks that nothing crashes
     // compares to mllib and fails every now and then because we do not consider the margin
     public void testTextIndex() throws IOException, JAXBException, SAXException {
@@ -139,6 +142,7 @@ public class PMMLTests extends ESTestCase {
     }
 
     @Test
+    @AwaitsFix(bugUrl = "")
     // only just checks that nothing crashes
     // compares to mllib and fails every now and then because we do not consider the margin
     public void checkMLlibPMMLOutputValid() throws IOException, JAXBException, SAXException {
@@ -164,4 +168,112 @@ public class PMMLTests extends ESTestCase {
             }
         }
     }
+
+    @Test
+    // only just checks that nothing crashes
+    // compares to mllib and fails every now and then because we do not consider the margin
+    public void testMLLibVsEsSVM() throws IOException, JAXBException, SAXException {
+        for (int i = 0; i < 100; i++) {
+
+            double[] modelParams = {randomFloat() * randomIntBetween(-100, +100), randomFloat() * randomIntBetween(-100, +100), randomFloat() * randomIntBetween(-100, +100)};
+            SVMModel svmm = new SVMModel(new DenseVector(modelParams), 0.1);
+            String pmmlString = "<PMML xmlns=\"http://www.dmg.org/PMML-4_2\">\n" +
+                    "    <Header description=\"linear SVM\">\n" +
+                    "        <Application name=\"Apache Spark MLlib\" version=\"1.5.2\"/>\n" +
+                    "        <Timestamp>2015-12-30T13:51:42</Timestamp>\n" +
+                    "    </Header>\n" +
+                    "    <DataDictionary numberOfFields=\"4\">\n" +
+                    "        <DataField name=\"field_0\" optype=\"continuous\" dataType=\"double\"/>\n" +
+                    "        <DataField name=\"field_1\" optype=\"continuous\" dataType=\"double\"/>\n" +
+                    "        <DataField name=\"field_2\" optype=\"continuous\" dataType=\"double\"/>\n" +
+                    "        <DataField name=\"target\" optype=\"categorical\" dataType=\"string\"/>\n" +
+                    "    </DataDictionary>\n" +
+                    "    <RegressionModel modelName=\"linear SVM\" functionName=\"classification\" normalizationMethod=\"none\">\n" +
+                    "        <MiningSchema>\n" +
+                    "            <MiningField name=\"field_0\" usageType=\"active\"/>\n" +
+                    "            <MiningField name=\"field_1\" usageType=\"active\"/>\n" +
+                    "            <MiningField name=\"field_2\" usageType=\"active\"/>\n" +
+                    "            <MiningField name=\"target\" usageType=\"target\"/>\n" +
+                    "        </MiningSchema>\n" +
+                    "        <RegressionTable intercept=\"0.1\" targetCategory=\"1\">\n" +
+                    "            <NumericPredictor name=\"field_0\" coefficient=\"" + modelParams[0] + "\"/>\n" +
+                    "            <NumericPredictor name=\"field_1\" coefficient=\"" + modelParams[1] + "\"/>\n" +
+                    "            <NumericPredictor name=\"field_2\" coefficient=\"" + modelParams[2] + "\"/>\n" +
+                    "        </RegressionTable>\n" +
+                    "        <RegressionTable intercept=\"0.0\" targetCategory=\"0\"/>\n" +
+                    "    </RegressionModel>\n" +
+                    "</PMML>";
+
+            PMML pmml;
+            try (InputStream is = new ByteArrayInputStream(pmmlString.getBytes(Charset.defaultCharset()))) {
+                Source transformedSource = ImportFilter.apply(new InputSource(is));
+                pmml = JAXBUtil.unmarshalPMML(transformedSource);
+            }
+            EsLinearSVMModel esLinearSVMModel = new EsLinearSVMModel((RegressionModel) pmml.getModels().get(0));
+            Map<FieldName, Object> params = new HashMap<>();
+            int[] vals = new int[]{1, 1, 1, 0};//{randomIntBetween(0, +100), randomIntBetween(0, +100), randomIntBetween(0, +100), 0};
+            params.put(new FieldName("field_0"), new Double(vals[0]));
+            params.put(new FieldName("field_1"), new Double(vals[1]));
+            params.put(new FieldName("field_2"), new Double(vals[2]));
+            String result = esLinearSVMModel.evaluate(new Tuple<>(new int[]{0, 1, 2}, new double[]{vals[0], vals[1], vals[2]}));
+            double mllibResult = svmm.predict(new DenseVector(new double[]{vals[0], vals[1], vals[2]}));
+            assertThat(mllibResult, equalTo(Double.parseDouble(result)));
+        }
+    }
+
+
+    @Test
+    // only just checks that nothing crashes
+    public void testMLLibVsEsLLR() throws IOException, JAXBException, SAXException {
+        for (int i = 0; i < 10; i++) {
+
+            double[] modelParams = new double[]{randomFloat() * randomIntBetween(-100, +100), randomFloat() * randomIntBetween(-100, +100), randomFloat() * randomIntBetween(-100, +100), randomFloat() * randomIntBetween(-100, +100)};
+            LogisticRegressionModel lrm = new LogisticRegressionModel(new DenseVector(modelParams), 0.1);
+            String pmmlString = "<PMML xmlns=\"http://www.dmg.org/PMML-4_2\">\n" +
+                    "    <Header description=\"logistic regression\">\n" +
+                    "        <Application name=\"Apache Spark MLlib\" version=\"1.5.2\"/>\n" +
+                    "        <Timestamp>2016-01-02T03:57:37</Timestamp>\n" +
+                    "    </Header>\n" +
+                    "    <DataDictionary numberOfFields=\"4\">\n" +
+                    "        <DataField name=\"field_0\" optype=\"continuous\" dataType=\"double\"/>\n" +
+                    "        <DataField name=\"field_1\" optype=\"continuous\" dataType=\"double\"/>\n" +
+                    "        <DataField name=\"field_2\" optype=\"continuous\" dataType=\"double\"/>\n" +
+                    "        <DataField name=\"field_3\" optype=\"continuous\" dataType=\"double\"/>\n" +
+                    "        <DataField name=\"target\" optype=\"categorical\" dataType=\"string\"/>\n" +
+                    "    </DataDictionary>\n" +
+                    "    <RegressionModel modelName=\"logistic regression\" functionName=\"classification\" normalizationMethod=\"logit\">\n" +
+                    "        <MiningSchema>\n" +
+                    "            <MiningField name=\"field_0\" usageType=\"active\"/>\n" +
+                    "            <MiningField name=\"field_1\" usageType=\"active\"/>\n" +
+                    "            <MiningField name=\"field_2\" usageType=\"active\"/>\n" +
+                    "            <MiningField name=\"field_3\" usageType=\"active\"/>\n" +
+                    "            <MiningField name=\"target\" usageType=\"target\"/>\n" +
+                    "        </MiningSchema>\n" +
+                    "        <RegressionTable intercept=\"0.1\" targetCategory=\"1\">\n" +
+                    "            <NumericPredictor name=\"field_0\" coefficient=\"" + modelParams[0] + "\"/>\n" +
+                    "            <NumericPredictor name=\"field_1\" coefficient=\"" + modelParams[1] + "\"/>\n" +
+                    "            <NumericPredictor name=\"field_2\" coefficient=\"" + modelParams[2] + "\"/>\n" +
+                    "            <NumericPredictor name=\"field_3\" coefficient=\"" + modelParams[3] + "\"/>\n" +
+                    "        </RegressionTable>\n" +
+                    "        <RegressionTable intercept=\"-0.0\" targetCategory=\"0\"/>\n" +
+                    "    </RegressionModel>\n" +
+                    "</PMML>";
+            PMML pmml;
+            try (InputStream is = new ByteArrayInputStream(pmmlString.getBytes(Charset.defaultCharset()))) {
+                Source transformedSource = ImportFilter.apply(new InputSource(is));
+                pmml = JAXBUtil.unmarshalPMML(transformedSource);
+            }
+            EsLogisticRegressionModel esLogisticRegressionModel = new EsLogisticRegressionModel((RegressionModel) (pmml.getModels().get(0)));
+            Map<FieldName, Object> params = new HashMap<>();
+            int[] vals = new int[]{1, 1, 1, 0};//{randomIntBetween(0, +100), randomIntBetween(0, +100), randomIntBetween(0, +100), 0};
+            params.put(new FieldName("field_0"), new Double(vals[0]));
+            params.put(new FieldName("field_1"), new Double(vals[1]));
+            params.put(new FieldName("field_2"), new Double(vals[2]));
+            params.put(new FieldName("field_3"), new Double(vals[3]));
+            double mllibResult = lrm.predict(new DenseVector(new double[]{vals[0], vals[1], vals[2], vals[3]}));
+            String result = esLogisticRegressionModel.evaluate(new Tuple<>(new int[]{0, 1, 2}, new double[]{vals[0], vals[1], vals[2]}));
+            assertThat(mllibResult, equalTo(Double.parseDouble(result)));
+        }
+    }
+
 }
