fecf15042fbbef9bc2f0f7a4d42dfc1ddc7d8bca
Britta Weber <britta.weber@elasticsearch.com>
2016-06-17 18:06:25 +0200

    add minltinom and neural net

--- src/test/resources/r-scripts/workflow.R
+++ src/test/resources/r-scripts/workflow.R
@@ -6,6 +6,7 @@ library(graphics)
 
 library(rpart)
 library(e1071)
+library(nnet)
 # read and clean data
 mydata <- read.csv("/home/britta/es-token-plugin/src/test/resources/org/elasticsearch/script/adult.data", stringsAsFactors=FALSE, na.strings = c("")) #, check.names=FALSE)
 mydata$workclass<-replace(mydata$workclass,which(is.na(mydata$workclass)), rep("too-cool-to-work", sum(is.na(mydata$workclass))))
@@ -116,3 +117,52 @@ result<-data.frame(result)
 
 cMatrix <-confusionMatrix(result$result, testing$class, positive = NULL, 
                           dnn = c("result", "class"))
+
+
+myMultinNom <- multinom(class ~ age + workclass + education_num + marital_status + occupation + relationship + race + sex + hours_per_week 
+                        + native_country, data = training)
+
+
+
+
+
+
+
+
+
+
+
+# Neural Network
+
+
+# read and clean data
+mydata <- read.csv("/home/britta/es-token-plugin/src/test/resources/org/elasticsearch/script/adult.data", stringsAsFactors=FALSE, na.strings = c("")) #, check.names=FALSE)
+mydata$workclass<-replace(mydata$workclass,which(is.na(mydata$workclass)), rep("too-cool-to-work", sum(is.na(mydata$workclass))))
+mydata$occupation<-replace(mydata$occupation,which(is.na(mydata$occupation)), rep("hedonist", sum(is.na(mydata$occupation))))
+mydata$native_country<-replace(mydata$native_country,which(is.na(mydata$native_country)), rep("Fiji", sum(is.na(mydata$native_country))))
+mydata$native_country[which(mydata$native_country == "Holand-Netherlands")]<-"England"
+# make class a factor
+mydata$class <- factor(mydata$class)
+mydata$education <- factor(mydata$education)
+mydata$marital_status <- factor(mydata$marital_status)
+mydata$occupation <- factor(mydata$occupation)
+mydata$relationship <- factor(mydata$relationship)
+mydata$race <- factor(mydata$race)
+mydata$sex <- factor(mydata$sex)
+mydata$native_country <- factor(mydata$native_country)
+mydata$workclass <- factor(mydata$workclass)
+
+# split into test and train set
+intrain<-createDataPartition(y=mydata$class,p=0.5,list=FALSE)
+training<-mydata[intrain,]
+
+testing<-mydata[-intrain,]
+
+
+myNN <- nnet(class ~ age + workclass + education + occupation + race + sex + hours_per_week + marital_status + relationship 
+             + native_country, size=10, data=training,maxit=1000)
+result  <- predict(myNN, testing)
+result<-sapply(result, function(x)if(x>0.5){">50K"}else{"<=50K"})
+result<-data.frame(result)
+cMatrix <-confusionMatrix(result$result, testing$class, positive = NULL, 
+                                                    dnn = c("result", "class"))
