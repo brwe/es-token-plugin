bb1f8d32e12d24763f71dc358dbdc6212faa94cf
Britta Weber <britta.weber@elasticsearch.com>
2016-06-24 00:23:21 +0200

    workflow examples

--- src/test/resources/r-scripts/dataHelperFunctions.R
+++ src/test/resources/r-scripts/dataHelperFunctions.R
@@ -1,20 +1,39 @@
 
-loadData<-function() {
-  script.dir <- getSrcDirectory(function(x) {x})
-  rmlist=(ls())
-  adult <- paste(script.dir, "/../org/elasticsearch/script/adult.data", sep="")
-  data <- read.csv(adult, header = TRUE, stringsAsFactors=FALSE, na.strings = c(""))
+
+loadData <- function() {
+  script.dir <- getSrcDirectory(function(x) {
+    x
+  })
+  rmlist = (ls())
+  adult <-
+    paste(script.dir,
+          "/../org/elasticsearch/script/adult.data",
+          sep = "")
+  data <-
+    read.csv(
+      adult,
+      header = TRUE,
+      stringsAsFactors = FALSE,
+      na.strings = c("")
+    )
   return(data)
 }
 
-replaceMissingValues<- function(data) {
-  data$workclass<-replace(data$workclass,which(is.na(data$workclass)), rep("too-cool-to-work", sum(is.na(data$workclass))))
-  data$occupation<-replace(data$occupation,which(is.na(data$occupation)), rep("hedonist", sum(is.na(data$occupation))))
-  data$native_country<-replace(data$native_country,which(is.na(data$native_country)), rep("Fiji", sum(is.na(data$native_country))))
+replaceMissingValues <- function(data) {
+  data$workclass <-
+    replace(data$workclass,
+            which(is.na(data$workclass)),
+            rep("too-cool-to-work", sum(is.na(data$workclass))))
+  data$occupation <-
+    replace(data$occupation, which(is.na(data$occupation)), rep("hedonist", sum(is.na(data$occupation))))
+  data$native_country <-
+    replace(data$native_country, which(is.na(data$native_country)), rep("Fiji", sum(is.na(
+      data$native_country
+    ))))
   return(data)
 }
 
-convertStringsToFactors<-function(data) {
+convertStringsToFactors <- function(data) {
   data$class <- factor(data$class)
   data$education <- factor(data$education)
   data$marital_status <- factor(data$marital_status)
@@ -27,18 +46,18 @@ convertStringsToFactors<-function(data) {
   return(data)
 }
 
-prepareData<-function() {
-  data<-loadData()
-  data<-replaceMissingValues(data)
-  data<-convertStringsToFactors(data)
+prepareData <- function() {
+  data <- loadData()
+  data <- replaceMissingValues(data)
+  data <- convertStringsToFactors(data)
   return(data)
 }
 
-normalizeNumberVariables<- function(data) {
-  data$age<-scale(data$age)
-  data$fnlwgt<-scale(data$fnlwgt)
-  data$capital_gain<-scale(data$capital_gain)
-  data$capital_loss<-scale(data$capital_loss)
-  data$hours_per_week<-scale(data$hours_per_week)
+normalizeNumberVariables <- function(data) {
+  data$age <- scale(data$age)
+  data$fnlwgt <- scale(data$fnlwgt)
+  data$capital_gain <- scale(data$capital_gain)
+  data$capital_loss <- scale(data$capital_loss)
+  data$hours_per_week <- scale(data$hours_per_week)
   return(data)
 }
--- src/test/resources/r-scripts/workflow.R
+++ src/test/resources/r-scripts/workflow.R
@@ -7,140 +7,168 @@ library(graphics)
 library(rpart)
 library(e1071)
 library(nnet)
+library(ada)
 
-
-source("/home/britta/es-token-plugin/src/test/resources/r-scripts/dataHelperFunctions.R")
-
-data<-prepareData()
-
-# split into test and train set
-intrain<-createDataPartition(y=data$class,p=0.5,list=FALSE)
-training<-data[intrain,]
-
-testing<-data[-intrain,]
-
-# train model
-mylogit <- glm(class ~ age + hours_per_week + workclass + education + education_num + marital_status + occupation + relationship + race + sex +native_country + hours_per_week , 
-               data = training, family = "binomial")
-
-prob <-predict(mylogit, newdata = testing, type = "response")
-result<-sapply(prob, function(x)if(x>0.5){">50K"}else{"<=50K"})
-result<-data.frame(result)
-confusionMatrix(result$result, testing$class, positive = NULL, 
-                dnn = c("result", "class"))
-
-# find we can remove education_num
-
-mylogit <- glm(class ~ age + hours_per_week + workclass + education  + marital_status + occupation + relationship + race + sex +native_country + hours_per_week , 
-               data = training, family = "binomial")
-
-prob <-predict(mylogit, newdata = testing, type = "response")
-result<-sapply(prob, function(x)if(x>0.5){">50K"}else{"<=50K"})
-result<-data.frame(result)
-confusionMatrix(result$result, testing$class, positive = NULL, 
-                dnn = c("result", "class"))
-
-
-# train tree model
-myTree <- rpart(class ~ age + workclass + education + education_num + marital_status + occupation + relationship + race + sex + hours_per_week + native_country , 
-                data = training, na.action = na.rpart)
-result <-predict(myTree, newdata = testing, type = "class")
-result<-data.frame(result)
-confusionMatrix(result$result, testing$class, positive = NULL, 
-                dnn = c("result", "class"))
-
-
-# try svm
-mySVM <- svm(class ~ age + workclass + education + education_num + marital_status + occupation + relationship + race + sex + hours_per_week + native_country, data = training, cost = 100, gamma = 1)
-result  <- predict(mySVM, testing)
-result<-data.frame(result)
-
-confusionMatrix(result$result, testing$class, positive = NULL, 
-                                dnn = c("result", "class"))
-
-# that did not go well...
-
-
-intrain<-createDataPartition(y=data$class,p=0.5,list=FALSE)
-training<-data[intrain,]
-
-testing<-data[-intrain,]
-accuracies <- vector(mode="double", length=20)
-
-gamma<-0.0001
-
-for(i in c(1:4)) {
-  cparam<-1
-  for(j in c(1:4)){
-    print(paste("starting with gamma ", gamma, " and C ", cparam, sep=" "))
-    mySVM <- svm(class ~ age + workclass + education_num + marital_status + occupation + relationship + race + sex + hours_per_week 
-             + native_country, data = training, cost = cparam, gamma = gamma)
-    result  <- predict(mySVM, testing)
-    result<-data.frame(result)
-
-    cMatrix <-confusionMatrix(result$result, testing$class, positive = NULL, 
-                dnn = c("result", "class"))
-
-    accuracies[i]<-cMatrix$overall["Accuracy"]
-
-    cparam=cparam*10
-    print(paste("done with gamma ", gamma, " and C, accuracy is ", accuracies[i], cparam, sep=" "))
-  }
-  gamma=gamma*10
-}
-# that is better but still not super...
-print(accuracies)
-
+script.dir <- getSrcDirectory(function(x) {
+  x
+})
+rmlist = (ls())
+script <- paste(script.dir, "/dataHelperFunctions.R", sep = "")
+result.dir <-
+  paste(script.dir, "/../org/elasticsearch/script/", sep = "")
+source(script)
 
 
 # try naive bayes
+# load data and replace missing values
+data <- prepareData()
 
-data<- prepareData();
+intrain <-
+  createDataPartition(y = data$class, p = 0.5, list = FALSE)
+training <- data[intrain, ]
+testing <- data[-intrain, ]
 
-intrain<-createDataPartition(y=data$class,p=0.5,list=FALSE)
-training<-data[intrain,]
-testing<-data[-intrain,]
-
-myNB <- naiveBayes(class ~ age + workclass + education_num + marital_status + occupation + relationship + race + sex + hours_per_week 
-             + native_country, data = training)
+myNB <- naiveBayes(
+  class ~ age + workclass + education_num
+  + marital_status + occupation + relationship
+  + race + sex + hours_per_week
+  + native_country + capital_loss + capital_gain,
+  data = training
+)
 result  <- predict(myNB, testing)
-result<-data.frame(result)
+result <- data.frame(result)
 
-cMatrix <-confusionMatrix(result$result, testing$class, positive = NULL, 
-                          dnn = c("result", "class"))
+cMatrixNB <-
+  confusionMatrix(
+    result$result,
+    testing$class,
+    positive = NULL,
+    dnn = c("result", "class")
+  )
 
-# Neural Network
+pmmlModel <- pmml(myNB, predictedField = "class")
+write(toString.XMLNode(pmmlModel), 
+      file = "/Users/britta/poc/example-elasticsearch/elasticsearch-2.2.1/config/scripts/naive_bayes.pmml_model")
 
+# train logistic regression model
+mylogit <- glm(
+  class ~ age + workclass + education 
+  + marital_status + occupation + relationship
+  + race + sex + hours_per_week
+  + native_country + capital_loss + capital_gain,
+  data = training,
+  family = "binomial"
+)
 
-# read and clean data
-data<- prepareData();
+prob <- predict(mylogit, newdata = testing, type = "response")
+result <- sapply(prob, function(x)
+  if (x > 0.5) {
+    ">50K"
+  } else{
+    "<=50K"
+  })
+result <- data.frame(result)
+cMatrixLR <-
+  confusionMatrix(
+    result$result,
+    testing$class,
+    positive = NULL,
+    dnn = c("result", "class")
+  )
+pmmlModel <- pmml(mylogit)
+write(toString.XMLNode(pmmlModel), file = "/Users/britta/poc/example-elasticsearch/elasticsearch-2.2.1/config/scripts/logistic.pmml_model")
 
-intrain<-createDataPartition(y=data$class,p=0.5,list=FALSE)
-training<-data[intrain,]
+# don't want to spend more time on it
 
-testing<-data[-intrain,]
-myNN <- nnet(class ~ age + workclass + education + occupation + race + sex + hours_per_week + marital_status + relationship 
-             + native_country, size=10, data=training,maxit=1000)
+# try neural network
+myNN <-
+  nnet(
+    class ~ age + workclass + education + occupation + race + sex + hours_per_week + marital_status + relationship
+    + native_country,
+    size = 10,
+    data = training,
+    maxit = 1000
+  )
 result  <- predict(myNN, testing)
-result<-sapply(result, function(x)if(x>0.5){">50K"}else{"<=50K"})
-result<-data.frame(result)
-cMatrix <-confusionMatrix(result$result, testing$class, positive = NULL, 
-                          dnn = c("result", "class"))
-
-data<-normalizeNumberVariables(data)
-intrain<-createDataPartition(y=data$class,p=0.5,list=FALSE)
-
-training<-data[intrain,]
-
-testing<-data[-intrain,]
-
-
-myNN <- nnet(class ~ age + workclass + education + occupation + race + sex + hours_per_week + marital_status + relationship 
-             + native_country, size=10, data=training,maxit=1000)
-result  <- predict(myNN, testing)
-result<-sapply(result, function(x)if(x>0.5){">50K"}else{"<=50K"})
-result<-data.frame(result)
-cMatrix <-confusionMatrix(result$result, testing$class, positive = NULL, 
-                          dnn = c("result", "class"))
+result <- sapply(result, function(x)
+  if (x > 0.5) {
+    ">50K"
+  } else{
+    "<=50K"
+  })
+result <- data.frame(result)
+cMatrixNN <-
+  confusionMatrix(
+    result$result,
+    testing$class,
+    positive = NULL,
+    dnn = c("result", "class")
+  )
 
 
+# train tree model
+myTree <- rpart(
+  class ~ age + workclass + education + education_num
+  + marital_status + occupation + relationship
+  + race + sex + hours_per_week
+  + native_country + capital_loss + capital_gain,
+  data = training,
+  na.action = na.rpart
+)
+result <- predict(myTree, newdata = testing, type = "class")
+result <- data.frame(result)
+cMatrixTree <-
+  confusionMatrix(
+    result$result,
+    testing$class,
+    positive = NULL,
+    dnn = c("result", "class")
+  )
+pmmlModel<-pmml(myTree)
+write(toString.XMLNode(pmmlModel), file = "/Users/britta/poc/example-elasticsearch/elasticsearch-2.2.1/config/scripts/tree.pmml_model")
+
+# try with boosting
+training <- data[intrain,]
+testing <- data[-intrain,]
+mydata2 <- training[,!(names(training) %in% c("class"))]
+myAda <- ada(mydata2, training$class)
+result  <- predict(myAda, testing)
+result <- data.frame(result)
+cMatrixAda <-
+  confusionMatrix(
+    result$result,
+    testing$class,
+    positive = NULL,
+    dnn = c("result", "class")
+  )
+
+
+results <-
+  data.frame(
+    cbind(
+      cMatrixNB$byClass,
+      cMatrixLR$byClass,
+      cMatrixNN$byClass,
+      cMatrixTree$byClass,
+      cMatrixAda$byClass
+    )
+  )
+colnames(results) <-
+  c("Naive Bayes",
+    "Logistic Regression",
+    "Neural Network",
+    "Tree",
+    "Ada")
+
+# try svm
+# or maybe not because it takes forever...
+# mySVM <- svm(class ~ age + workclass + education + education_num
+#              + marital_status + occupation + relationship
+#              + race + sex + hours_per_week
+#              + native_country + capital_loss + capital_gain
+#              , data = training, cost = 100, gamma = 1)
+# result  <- predict(mySVM, testing)
+# result<-data.frame(result)
+#
+# confusionMatrix(result$result, testing$class, positive = NULL,
+#                                 dnn = c("result", "class"))
